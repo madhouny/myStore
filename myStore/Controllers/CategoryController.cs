a23f5a72bcdb4f2cf6f94d5e3b3681da7317eda8
madhouny <younes.madhoun@gmail.com>
2020-11-08 13:36:59 +0100

    CRUD Category

---
+++ myStore/Controllers/CategoryController.cs
@@ -0,0 +1,65 @@
+﻿using myStore.Models;
+using myStore.myStoreServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        //Injection de Service Categories , pour separer Controller et Database
+        CategoriesService categoryService = new CategoriesService();
+
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var categories = categoryService.GetCategories();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            categoryService.SaveCategory(category);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int Id)
+        {
+            var category = categoryService.GetCategories(Id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            categoryService.UpdateCategory(category);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int Id)
+        {
+            var category = categoryService.GetCategories(Id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Category category)
+        {
+            categoryService.DeleteCategory(category.ID);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ myStore/myStoreServices/CategoriesService.cs
@@ -0,0 +1,56 @@
+﻿using myStore.DAL;
+using myStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace myStore.myStoreServices
+{
+    public class CategoriesService
+    {
+        public Category GetCategories(int Id)
+        {
+            using (var context = new StoreContext())
+            {
+                return context.Categories.Find(Id);
+            }
+        }
+
+        public List<Category> GetCategories()
+        {
+            using (var context = new StoreContext())
+            {
+                return context.Categories.ToList();
+            }
+        }
+
+        public void SaveCategory(Category category)
+        {
+            using(var context = new StoreContext())
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            using (var context = new StoreContext())
+            {
+                context.Entry(category).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteCategory(int Id)
+        {
+            using (var context = new StoreContext())
+            {
+                var category = context.Categories.Find(Id);
+                context.Categories.Remove(category);
+                context.SaveChanges();
+            }
+        }
+    }
+}
