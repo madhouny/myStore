1ac2740a04a937fbb41707865ee3f5c1b7f4287f
madhouny <younes.madhoun@gmail.com>
2020-11-22 15:45:31 +0100

    User Authentification (Register, Login)

---
+++ myStore/Migrations/202011220923582_UserModel.cs
@@ -0,0 +1,30 @@
+﻿namespace myStore.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UserModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserID = c.Int(nullable: false, identity: true),
+                        Username = c.String(nullable: false),
+                        Password = c.String(nullable: false),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Email = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.UserID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
